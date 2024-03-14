5e29926feee9441cf365ed5c9f44f1b419dcda81
Carlos Cotera <carlos97jkl@hotmail.com>
2024-03-14 00:42:06 -0500

    feat: testing git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            alt="test branch"
           >
             By{" "}
             <Image
