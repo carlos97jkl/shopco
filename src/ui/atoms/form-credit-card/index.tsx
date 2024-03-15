e9823d32c67e213728cbb187a58f0a8142cb3b65
Carlos Cotera <carlos97jkl@hotmail.com>
2024-03-15 16:36:00 -0500

    feat: Adding form payment data, adding summary, logic stepper and credit card validation with logo

---
+++ src/ui/atoms/form-credit-card/index.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { Grid, TextField } from "@mui/material";
+import { useState } from "react";
+import { usePaymentInputs } from "react-payment-inputs";
+import images from "react-payment-inputs/images";
+
+const FormCreditCard = () => {
+  const {
+    getCardNumberProps,
+    getExpiryDateProps,
+    getCVCProps,
+    getCardImageProps,
+  } = usePaymentInputs();
+  const [cardNumber, setCardNumber] = useState();
+  const [experyDate, setExperyDate] = useState();
+  const [securityCode, setSecurityCode] = useState();
+  const handleChangeCardNumber = (event: any) => {
+    setCardNumber(event?.target?.value);
+  };
+  return (
+    <Grid container gap={3}>
+      <Grid item xs={12}>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <svg
+                {...getCardImageProps({ images })}
+                style={{ paddingRight: "5px" }}
+              />
+            ),
+          }}
+          inputProps={{
+            value: cardNumber,
+            ...getCardNumberProps({
+              onChange: handleChangeCardNumber,
+            }),
+          }}
+          fullWidth
+          size="small"
+          variant="outlined"
+          label="Credit card number"
+        />
+      </Grid>
+      <Grid item container justifyContent="space-between">
+        <Grid item xs={3}>
+          <TextField
+            inputProps={{
+              value: experyDate,
+              ...getExpiryDateProps({
+                onChange: (event: any) => setExperyDate(event.target.value),
+              }),
+            }}
+            size="small"
+            fullWidth
+            variant="outlined"
+            label="Expery date"
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <TextField
+            inputProps={{
+              value: securityCode,
+              ...getCVCProps({
+                onChange: (event: any) => setSecurityCode(event.target.value),
+              }),
+            }}
+            size="small"
+            fullWidth
+            variant="outlined"
+            label="Security code (CVC)"
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          size="small"
+          fullWidth
+          variant="outlined"
+          label="Cardholder's name"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          size="small"
+          fullWidth
+          variant="outlined"
+          label="Cardholder ID"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          size="small"
+          fullWidth
+          variant="outlined"
+          label="Number of payments"
+        />
+      </Grid>
+    </Grid>
+  );
+};
+export default FormCreditCard;
---
+++ src/ui/molecules/modal-checkout/index.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+// @packages
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import {
+  Button,
+  Grid,
+  Step,
+  StepButton,
+  StepContent,
+  Stepper,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import FormCreditCard from "@/ui/atoms/form-credit-card";
+import BuySummary from "@/ui/atoms/buy-summary";
+
+type TModalCheckout = {
+  isOpenModal: boolean;
+  setIsOpenModal: (isOpenModal: boolean) => void;
+};
+
+const ModalCheckout = ({ isOpenModal, setIsOpenModal }: TModalCheckout) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = [
+    { title: "Payment Data", component: () => <FormCreditCard /> },
+    { title: "Summary", component: () => <BuySummary /> },
+  ];
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  console.log(activeStep);
+
+  const handleStep = (step: number) => () => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Dialog open={isOpenModal} maxWidth="sm" fullWidth>
+      <DialogTitle>
+        <Typography variant="h5">Payment Checkout</Typography>
+      </DialogTitle>
+      <DialogContent style={{ height: "400px" }}>
+        <Stepper nonLinear activeStep={activeStep}>
+          {steps.map(({ title }, index) => (
+            <Step key={title} completed={activeStep > index}>
+              <StepButton color="inherit" onClick={handleStep(index)}>
+                {title}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+        {steps.map(({ title, component }, index) => (
+          <>
+            {index === activeStep && (
+              <Grid p="20px" key={title}>
+                {component()}
+              </Grid>
+            )}
+          </>
+        ))}
+      </DialogContent>
+      <Grid
+        container
+        justifyContent="space-between"
+        style={{ padding: "10px 20px" }}
+      >
+        <Button disabled={activeStep === 0} onClick={handleBack}>
+          <KeyboardArrowLeft /> Back
+        </Button>
+        <Button
+          disabled={activeStep === steps.length - 1}
+          onClick={handleStep(activeStep + 1)}
+        >
+          Next <KeyboardArrowRight />
+        </Button>
+      </Grid>
+    </Dialog>
+  );
+};
+
+export default ModalCheckout;
